1f35da597411772b04261e08baf8f5d5a7d178bd
Brian Y <yos314@gmail.com>
2016-01-19 21:32:16 -0900

    Ep 3 & 4 done

---
+++ src/main/java/mod/yos/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,32 @@
+package mod.yos.mc_lmr;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="LetsModReboot", name="Lets Mod Reboot", version="1.8.9-0.0.1")
+
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
