dc59d34bebf3332eb22139b63787b61e70e07487
Brian Y <yos314@gmail.com>
2016-01-20 21:57:45 -0900

    added object holder

--- src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
+++ src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
@@ -3,8 +3,9 @@ package yos.mod.letsmodreboot.init;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import yos.mod.letsmodreboot.block.BlockFlag;
 import yos.mod.letsmodreboot.block.BlockLMR;
+import yos.mod.letsmodreboot.reference.Reference;
 
-
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockLMR flag = new BlockFlag();
 
--- src/main/java/yos/mod/letsmodreboot/init/ModItems.java
+++ src/main/java/yos/mod/letsmodreboot/init/ModItems.java
@@ -4,7 +4,9 @@ package yos.mod.letsmodreboot.init;
 import yos.mod.letsmodreboot.item.ItemLMR;
 import yos.mod.letsmodreboot.item.ItemMapleLeaf;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import yos.mod.letsmodreboot.reference.Reference;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems {
     public static final ItemLMR mapleLeaf = new ItemMapleLeaf();
 
