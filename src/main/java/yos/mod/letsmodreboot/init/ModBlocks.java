c5880a6b7bf1fdc1bcb747ca6bb6584f9a6c5260
Brian Y <yos314@gmail.com>
2016-01-24 20:01:30 -0900

    got block texture working

--- src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
+++ src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
@@ -1,9 +1,13 @@
 package yos.mod.letsmodreboot.init;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import yos.mod.letsmodreboot.block.BlockFlag;
 import yos.mod.letsmodreboot.block.BlockLMR;
 import yos.mod.letsmodreboot.reference.Reference;
+import yos.mod.letsmodreboot.reference.Resources;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
@@ -11,5 +15,9 @@ public class ModBlocks {
 
     public static void init(){
         GameRegistry.registerBlock(flag, "flag");
+    }
+
+    public static void registerItemRenderer(){
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(flag), 0, new ModelResourceLocation(Resources.FLAG_TEXTURE, "inventory"));
     }
 }
--- src/main/java/yos/mod/letsmodreboot/proxy/ClientProxy.java
+++ src/main/java/yos/mod/letsmodreboot/proxy/ClientProxy.java
@@ -1,6 +1,7 @@
 package yos.mod.letsmodreboot.proxy;
 
 
+import yos.mod.letsmodreboot.init.ModBlocks;
 import yos.mod.letsmodreboot.init.ModItems;
 
 public class ClientProxy extends CommonProxy {
@@ -11,6 +12,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void init(){
         ModItems.registerItemRenderer();
+        ModBlocks.registerItemRenderer();
     }
 
     @Override
