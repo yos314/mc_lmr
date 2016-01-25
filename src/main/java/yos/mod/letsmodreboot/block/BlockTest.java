042dad6b244e36391b7c03da168a7b8356d86a5f
Brian Y <yos314@gmail.com>
2016-01-24 22:00:21 -0900

    Added test block

---
+++ src/main/java/yos/mod/letsmodreboot/block/BlockTest.java
@@ -0,0 +1,9 @@
+package yos.mod.letsmodreboot.block;
+
+
+public class BlockTest extends BlockLMR {
+    public BlockTest(){
+        super();
+        this.setUnlocalizedName("flag");
+    }
+}
--- src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
+++ src/main/java/yos/mod/letsmodreboot/init/ModBlocks.java
@@ -6,18 +6,22 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import yos.mod.letsmodreboot.block.BlockFlag;
 import yos.mod.letsmodreboot.block.BlockLMR;
+import yos.mod.letsmodreboot.block.BlockTest;
 import yos.mod.letsmodreboot.reference.Reference;
 import yos.mod.letsmodreboot.reference.Resources;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockLMR flag = new BlockFlag();
+    public static final BlockLMR test = new BlockTest();
 
     public static void init(){
         GameRegistry.registerBlock(flag, "flag");
+        GameRegistry.registerBlock(test, "test");
     }
 
     public static void registerItemRenderer(){
         Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(flag), 0, new ModelResourceLocation(Resources.FLAG_TEXTURE, "inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(test), 0, new ModelResourceLocation(Resources.TEST_TEXTURE, "inventory"));
     }
 }
