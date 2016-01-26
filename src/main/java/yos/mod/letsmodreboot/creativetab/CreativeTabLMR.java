12cf43ed2410ca03dc0a40e6ec3a471dfc1ce98a
Brian Y <yos314@gmail.com>
2016-01-25 18:17:33 -0900

    screwed up and fixed the localized name for the creative tab, now it is localization

--- src/main/java/yos/mod/letsmodreboot/creativetab/CreativeTabLMR.java
+++ src/main/java/yos/mod/letsmodreboot/creativetab/CreativeTabLMR.java
@@ -7,15 +7,10 @@ import yos.mod.letsmodreboot.init.ModItems;
 import yos.mod.letsmodreboot.reference.Reference;
 
 public class CreativeTabLMR {
-    public static final CreativeTabs LMR_TAB = new CreativeTabs(Reference.MOD_ID){
+    public static final CreativeTabs LMR_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase()){
         @Override
         public Item getTabIconItem(){
             return ModItems.mapleLeaf;
-        }
-
-        @Override
-        public String getTranslatedTabLabel(){
-            return "Let's Mod Reboot";
         }
 
     };
